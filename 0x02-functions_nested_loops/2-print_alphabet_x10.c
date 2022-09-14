a8ec564bccc2a67ed96cb399efd8ad4115f37868
RayBillion-s <iluoberaymond@gmail.com>
2022-09-14 15:00:10 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
-#include "main"
+#include "main.h"
 /**
- * print_alphabet_x10 - function to print abc 10 times
+ * print_alphabet_x10 - funtion to print abc 10 times
  *
  * Return: 0
  */
-void print_alphabet_x10(void)
+void print alphabet_x10(void)
 {
 	char c, i;
 
 	for (i = 0; i <= 9; i++)
 	{
-			for (c = 'a'; c <= 'z'; c++)
-			{
-				_putchar(c);
-			}
-			_putchar('\n');
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
 }
