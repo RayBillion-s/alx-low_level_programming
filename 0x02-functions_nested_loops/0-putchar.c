8d86d1f987f573e1878fb1067c46affe7b2633bd
RayBillion-s <iluoberaymond@gmail.com>
2022-09-14 08:18:21 -0700

    _putchar(

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  * Return: always 0
  */
 
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -15,8 +15,8 @@ int main (void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');
+	_putchar('\n);
 
 	return (0);
-
 }
+
