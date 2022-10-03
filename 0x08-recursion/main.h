ec84f90043e0dd465cd8831d935344a46660aa1c
RayBillion-s <iluoberaymond@gmail.com>
2022-10-03 02:51:42 -0700

    0-realupdate

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-int putchar(char c);
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -12,8 +12,8 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _sqrt(int n, int x);
-int is_prime_number(int n, int start);
-void check(char *s, int end, int *flag);
+int is_prime(int n, int start);
+void check(char *s, int start, int end, int *flag);
 int wildcmp(char *s1, char *s2);
 
 #endif /*MAIN_H*/
