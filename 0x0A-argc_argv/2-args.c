53c81641fc1b2f64f99b52a57158af8fa26c8ca3
RayBillion-s <iluoberaymond@gmail.com>
2022-10-06 23:26:52 -0700

    2-args.c update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,11 +10,11 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int arg;
 	
-	for(i=0; i < argc; i++)
+	for(arg=0; arg < argc; arg++)
 	{
-		printf(%s/n, argv[i]);
+		printf("%s/n", argv[arg]);
 	}
 	return (0);
 }
